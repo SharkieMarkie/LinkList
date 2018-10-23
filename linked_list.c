bc16c4fbb0f58f5b6c217ea2be275345c3bc5fa7
1500857 <e1500857@shell.vamk.fi>
2018-10-23 11:29:20 +0300

    first commit

---
+++ linked_list.c
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include "linked_list.h"
+
+/*********************************************************************
+Main
+**********************************************************************/
+typedef struct linked_list linked_list;
+
+int add_to_list(linked_list *ll, char *s){
+	
+	
+	
+	return 0;
+	
+	
+}
+
+
+int display_item(linked_list *ll)
+{
+	
+	
+	return 0;
+	
+	
+	
+}
+int display_list(linked_list *ll){
+	
+	return 0;
+	
+	
+}
+
+linked_list * search_from_list(linked_list *ll, char *s){
+	return NULL;
+}
+int delete_from_list(linked_list *ll, int index){
+	return -1;
+}
+int empty_list(linked_list *ll){
+	return -1;
+}
+int swap_items(linked_list *f, linked_list *s){
+	return -1;
+}
+int sort_list(linked_list *ll){
+	return -1;
+}
+int linkedlist_status(linked_list *ll){
+	return -1;
+}
